2691082191fe032e35538f9d3394cfd77742b892
FI-User <fi-user@Fiusers-MacBook-Pro.local>
2022-10-28 13:19:15 +0530

    changes Made

--- app/src/main/java/com/example/settingsdesign/MainActivity.kt
+++ app/src/main/java/com/example/settingsdesign/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.settingsdesign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var redirect:AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
     }
-
 
 }
--- app/src/main/java/com/example/settingsdesign/SettingsActivity.kt
+++ app/src/main/java/com/example/settingsdesign/SettingsActivity.kt
@@ -2,10 +2,21 @@ package com.example.settingsdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var submit_btn:AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        submit_btn=findViewById(R.id.submit_btn)
+        submit_btn.setOnClickListener {
+            Toast.makeText(applicationContext,"Successfully Updated", Toast.LENGTH_SHORT).show()
+
+        }
+
+
     }
 }
