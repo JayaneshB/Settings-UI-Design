f2cba3c769e855e0858fb5de19f1c7f11300c1e0
FI-User <fi-user@Fiusers-MacBook-Pro.local>
2022-10-28 15:31:37 +0530

    Changes Committed

--- app/src/main/java/com/example/settingsdesign/MainActivity.kt
+++ app/src/main/java/com/example/settingsdesign/MainActivity.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var redirect:AppCompatButton
+    private lateinit var redirect: AppCompatImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        redirect=findViewById(R.id.redirect)
+        redirect.setOnClickListener {
+            val intent= Intent(this,SettingsActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
--- app/src/main/java/com/example/settingsdesign/SettingsActivity.kt
+++ app/src/main/java/com/example/settingsdesign/SettingsActivity.kt
@@ -1,12 +1,15 @@
 package com.example.settingsdesign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
 
 class SettingsActivity : AppCompatActivity() {
-    private lateinit var submit_btn:AppCompatButton
+    private lateinit var submit_btn:AppCompatImageButton
+    private lateinit var prev_btn : AppCompatImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -14,7 +17,11 @@ class SettingsActivity : AppCompatActivity() {
         submit_btn=findViewById(R.id.submit_btn)
         submit_btn.setOnClickListener {
             Toast.makeText(applicationContext,"Successfully Updated", Toast.LENGTH_SHORT).show()
-
+        }
+        prev_btn=findViewById(R.id.back_btn)
+        prev_btn.setOnClickListener{
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
         }
 
 
