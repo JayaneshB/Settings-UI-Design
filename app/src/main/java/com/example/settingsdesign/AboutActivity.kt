e60bca723a25e3ffaf485a222f5f09211fedf821
FI-User <fi-user@Fiusers-MacBook-Pro.local>
2022-12-20 10:02:00 +0530

    Updated Changes

--- app/src/main/java/com/example/settingsdesign/AboutActivity.kt
+++ app/src/main/java/com/example/settingsdesign/AboutActivity.kt
@@ -7,19 +7,19 @@ import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageButton
 
-class SettingsActivity : AppCompatActivity() {
-    private lateinit var submit_btn:AppCompatImageButton
-    private lateinit var prev_btn : AppCompatImageButton
+class AboutActivity : AppCompatActivity() {
+    private lateinit var submit_btn_01:AppCompatImageButton
+    private lateinit var prev_btn_01 : AppCompatImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(R.layout.activity_about)
 
-        submit_btn=findViewById(R.id.submit_btn)
-        submit_btn.setOnClickListener {
+        submit_btn_01=findViewById(R.id.submit_btn_01)
+        submit_btn_01.setOnClickListener {
             Toast.makeText(applicationContext,"Successfully Updated", Toast.LENGTH_SHORT).show()
         }
-        prev_btn=findViewById(R.id.back_btn)
-        prev_btn.setOnClickListener{
+        prev_btn_01=findViewById(R.id.back_btn_01)
+        prev_btn_01.setOnClickListener{
             val intent= Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/settingsdesign/MainActivity.kt
+++ app/src/main/java/com/example/settingsdesign/MainActivity.kt
@@ -3,11 +3,11 @@ package com.example.settingsdesign
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageButton
 
 class MainActivity : AppCompatActivity() {
     private lateinit var redirect: AppCompatImageButton
+    private lateinit var redirect_page: AppCompatImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +18,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        redirect_page=findViewById(R.id.redirect_aboutpage)
+        redirect_page.setOnClickListener {
+            val intent= Intent(this,AboutActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
